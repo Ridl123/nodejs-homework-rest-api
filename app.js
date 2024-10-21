00d1d03d54b8282072adaeb3956d0ace3cd2b57a
Ridl123 <purdel_richard@yahoo.com>
2024-10-21 16:06:05 +0300

    1st try
    tried to fix some things from the previous branch as well

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-
-const contactsRouter = require("./routes/api/contacts");
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import contactsRouter from "./routes/api/contacts.js";
+import usersRouter from "./routes/api/users.js";
+import auth from "./middlewares/auth.middleware.js";
 
 const app = express();
 
@@ -12,7 +13,10 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", auth, contactsRouter);
+app.use("/api/users", usersRouter);
+
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -22,4 +26,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-module.exports = app;
+export default app;
